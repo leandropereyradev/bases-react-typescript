1305c755759ce4306b9289bce61aa2a0316b5c16
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-05-30 12:28:45 +0200

    useUsuarios

--- src/components/Usuarios.tsx
+++ src/components/Usuarios.tsx
@@ -1,32 +1,8 @@
-import { useEffect, useState, useRef } from "react";
-import { reqResApi } from "../api/reqRes";
-import { ReqResListado, Usuario } from "../interfaces/reqRes";
+import { useUsuarios } from "../hooks/useUsuarios";
+import { Usuario } from "../interfaces/reqRes";
 
 export const Usuarios = () => {
-  const [usuarios, setUsuarios] = useState<Usuario[]>();
-
-  const paginaRef = useRef(1);
-
-  useEffect(() => {
-    return () => {
-      cargarUsuarios();
-    };
-  }, []);
-
-  const cargarUsuarios = async () => {
-    const res = await reqResApi.get<ReqResListado>("/users", {
-      params: {
-        page: paginaRef.current,
-      },
-    });
-
-    if (res.data.data.length > 0) {
-      setUsuarios(res.data.data);
-      paginaRef.current++;
-    } else {
-      alert("No hay mas registros");
-    }
-  };
+  const { usuarios, cargarUsuarios } = useUsuarios();
 
   const renderItems = ({
     id,
